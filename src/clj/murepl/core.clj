259c220cf631eaf4fb94d38c2c42cdbf9f5fdecb
Andrew Roetker <andrew.roetker@puppetlabs.com>
2013-09-28 15:00:09 -0700

    Protocol and initial world

--- src/clj/murepl/core.clj
+++ src/clj/murepl/core.clj
@@ -1,6 +1,36 @@
 (ns murepl.core)
 
+
+(defn system
+  []
+  {:world (ref {})
+   :players (ref #{})}
+   :server nil
+  )
+
+(defn start
+  [{:keys [world players] :as system}]
+  (init-world world)
+)
+
+
+(defprotocol Player
+  [player]
+  (go [direction])
+  (look [])
+  (examine [object])
+  (say [something])
+  (yell [something])
+  (whisper [somebody something])
+  (take [object])
+  (inventory [])
+  (drop [])
+  (use [object action player])
+  (create-object [])
+  (create-room [])
+  (create-bot []))
+
 (defn foo
   "I don't do a whole lot."
   [x]
-  (println x "Hello, World!"))
+   x "Hello, World!")
