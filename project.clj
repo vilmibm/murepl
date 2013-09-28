3a7173cd4b95a2db1ef1dd14dfd35681838308e8
Nathaniel Smith <nathaniel@puppetlabs.com>
2013-09-28 12:05:26 -0400

    include compojure and himera

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
 (defproject murepl "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Multi User REPL"
+  :url "http://murepl.clojurecup.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-cljsbuild "0.3.3"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1889"]]
+                 [org.clojure/clojurescript "0.0-1889"]
+                 [compojure "1.1.5"]
+                 [himera "0.1.0-SNAPSHOT"]]
   :cljsbuild {:builds [{:source-paths ["src-cljs"]
                         :compiler {:output-to "resources/js/main.js"
                                    :pretty-print true
